55cb285dbb9e49c567c5a4de7913f46bfafebd93
AliMoradpour <Mr.mrdp@yahoo.com>
2022-12-11 13:43:30 -0800

    complete main page and complete the whole project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,11 @@
-// Popover
-const popoverTriggerList = document.querySelectorAll(
-  '[data-bs-toggle="popover"]'
-);
-const popoverList = [...popoverTriggerList].map(
-  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
-);
+// popover
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
 
 // Gender Select
-if (window.location.pathname === "/index.html") {
+if (window.location.pathname === "/") {
   const radioBtn1 = document.querySelector("#flexRadioDefault1");
   const radioBtn2 = document.querySelector("#flexRadioDefault2");
   const radioBtn3 = document.querySelector("#flexRadioDefault3");
@@ -16,11 +14,9 @@ if (window.location.pathname === "/index.html") {
   radioBtn1.addEventListener("change", () => {
     genderSelect.classList.add("d-none");
   });
-
   radioBtn2.addEventListener("change", () => {
     genderSelect.classList.add("d-none");
   });
-
   radioBtn3.addEventListener("change", () => {
     genderSelect.classList.remove("d-none");
   });
